a9246dae432b5e6708494431423f9279d1fedaa1
Vichithrakumar <tvichithrakumar@gmail.com>
2021-08-04 11:27:13 +0530

    Docker Setup

---
+++ dockersetup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt-get update -y
+apt-get install apt-transport-https -y
+apt-get install ca-certificates -y
+apt-get install curl -y
+apt-get install gnupg -y
+apt-get install lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+	  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+	    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose -y
